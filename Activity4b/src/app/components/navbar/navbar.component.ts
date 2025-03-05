2003d65d17ecc4e4f6199ab34b3756900d5bb6d2
JCagay-an <20221561@s.ubaguio.edu>
2025-03-05 11:54:26 +0800

    applied bootsrap components to home

--- Activity4b/src/app/components/navbar/navbar.component.ts
+++ Activity4b/src/app/components/navbar/navbar.component.ts
@@ -1,8 +1,10 @@
 import { Component } from '@angular/core';
+import { RouterModule } from '@angular/router';
+import { Router } from 'express';
 
 @Component({
   selector: 'app-navbar',
-  imports: [],
+  imports: [RouterModule],
   templateUrl: './navbar.component.html',
   styleUrl: './navbar.component.css'
 })
