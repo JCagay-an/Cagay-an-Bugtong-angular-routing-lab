e5c14bdff872844ceb79940d1c301603d1bdecf0
JCagay-an <20221561@s.ubaguio.edu>
2025-03-05 11:29:57 +0800

    Added navbar, footer, and header

--- Activity4b/src/app/app.component.ts
+++ Activity4b/src/app/app.component.ts
@@ -1,10 +1,13 @@
 import { Component } from '@angular/core';
 import { RouterModule } from '@angular/router';
+import { FooterComponent } from './components/footer/footer.component';
+import { HeaderComponent } from './components/header/header.component';
+import { NavbarComponent } from './components/navbar/navbar.component';
 
 @Component({
   selector: 'app-root',
   standalone: true,
-  imports: [RouterModule],
+  imports: [RouterModule, FooterComponent, HeaderComponent, NavbarComponent],
   templateUrl: './app.component.html',
   styleUrls: ['./app.component.css']
 })
