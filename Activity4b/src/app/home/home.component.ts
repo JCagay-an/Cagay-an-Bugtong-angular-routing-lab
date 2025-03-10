dcfa6dc763d75fee3b468c0c1ca96d131211e428
JCagay-an <20221561@s.ubaguio.edu>
2025-03-10 12:48:17 +0800

    LAST

--- Activity4b/src/app/home/home.component.ts
+++ Activity4b/src/app/home/home.component.ts
@@ -7,9 +7,5 @@ import { Component } from '@angular/core';
   styleUrl: './home.component.css'
 })
 export class HomeComponent {
-  messageVisible = false;
 
-  showMessage() {
-    this.messageVisible = !this.messageVisible;
-  }
 }
